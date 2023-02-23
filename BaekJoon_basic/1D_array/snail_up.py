6924f6d96c492995c0e335b3a386aced8f2c648f
97danielj <97danielj@gmail.com>
2023-02-23 18:53:38 +0900

    Codingtest 230223

--- BaekJoon_basic/1D_array/snail_up.py
+++ BaekJoon_basic/1D_array/snail_up.py
@@ -2,21 +2,21 @@
 # A : 아침 올라가는 거리
 # B : 밤에 내려오는 거리
 
+# 조건 : 1 ≤ B < A ≤ V ≤ 1,000,000,000
 
 A, B, V = list(map(int,input().split()))
 
-point = 0
-day_count = 0
-while True:
+dis_per_day = A-B # 하루 올라가는 거리
+dis_except_last_day = V-A #마
 
-    if point+A >= V:
-        day_count +=1
-        break
+day_count = dis_except_last_day // dis_per_day
 
-    point += (A-B)
+if day_count == 0:
     day_count += 1
-
+day_count += 1
 
 print(day_count)
+
+
 
 
