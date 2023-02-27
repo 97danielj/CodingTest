cbb2b41f845d92e6becf87d4141f88df73b073bb
97danielj <97danielj@gamil.com>
2023-02-27 20:56:15 +0900

    CodingTest 230227

--- BaekJoon_basic/basic_math1/ACM_hotel.py
+++ BaekJoon_basic/basic_math1/ACM_hotel.py
@@ -1,10 +1,22 @@
-t = int(input())
+# 전제조건 만약 1~6 이다 .그럼
+def solver(H, W, N):
 
-for i in range(t):
-    h, w, n = map(int, input().split())
-    num = n//h + 1 # 가로 이동 거리, 방번호
-    floor = n % h  # 위로 이동거리, 층 번호
-    if n % h == 0:  # h의 배수이면,
-        num = n//h
-        floor = h
-    print(f'{floor*100+num}')
+    num = N // H +1 # N 엘리베이터부터 거리, 호텔의 열 col
+    floor = N % H # 호텔의 층, row
+
+    if floor == 0:
+        num = N // H
+        floor = H
+
+    num = str(num)
+    if len(num) == 1:
+        num = '0' + num
+
+    room_assign = str(floor)+num
+    return room_assign
+
+T = int(input())
+
+for test_case in range(T):
+    H, W, N = map(int, input().split())
+    print(solver(H, W, N))
--- BaekJoon_basic/basic_math1/Become_president.py
+++ BaekJoon_basic/basic_math1/Become_president.py
@@ -1,6 +1,27 @@
 # 반상회
+# 제한 : 1 ≤ k, n ≤ 14
+def solver(k, n):
+    matrix = [[0 for j in range(n+1)] for i in range(k+1)] # (k+1) X (n+1) 행렬 생성
+
+    for floor in range(k+1):
+        for room_number in range(n+1):
+            if floor == 0:
+                matrix[0][room_number] = room_number
+            else:
+                room_sum = 0
+                for idx in range(room_number+1):
+                    room_sum += matrix[floor-1][idx]
+                matrix[floor][room_number] = room_sum
+
+    return matrix[k][n]
+
+
+
+
 
 T = int(input())
 
 for test_case in range(T):
-
+    k = int(input()) # 층 수
+    n = int(input()) # 호 수
+    print(solver(k, n))
