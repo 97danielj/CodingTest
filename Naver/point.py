bec4de1614fb60d950ed0813b9a491fc64a95e23
97danielj <97danielj@gmail.com>
2023-04-19 20:16:52 +0900

    Codingtest 230419

--- Naver/point.py
+++ Naver/point.py
@@ -6,7 +6,9 @@ mileage = 0
 total_charge = 0
 total_withdraw = 0
 
-
+from collections import deque
+buy_days_queue = deque()
+buy_price_queue = deque()
 
 def buy(x, price):
     global point
@@ -23,7 +25,7 @@ def buy(x, price):
         else:
             charge_count = rest_price // x + 1
             total_charge += (charge_count * x)
-            point += total_charge #충전한 금액을 포인트에
+            point += x*charge_count #충전한 금액을 포인트에
             point -= rest_price
 
 
@@ -45,28 +47,32 @@ def making_mileage(last_day_price):
 
 
 
-def solver(x, y, history):
-    last_buy_day = 0 # 최근 구매
-    last_buy_price = 0 # 최근 구매 가격
+def solver(x, y, pays):
+    global mileage
+    for pay in pays:
+        pay_list = pay.split()
+        while len(buy_days_queue) > 0 and buy_days_queue[0] + y <= int(pay_list[0]) and buy_days_queue[0] + y <= 30:
+            buy_days_queue.popleft()
+            target_price = buy_price_queue.popleft()
+            mileage += int(target_price*0.1)
 
-    for comm in history:
-        hist = comm.split()
-        current_day = int(hist[0]) #최근 내역 날짜
+        if int(pay_list[2]) > 0:
+            last_buy_day = int(pay_list[0])
+            last_buy_price = int(pay_list[2])
+            buy(x, last_buy_price)
+            buy_days_queue.append(last_buy_day)
+            buy_price_queue.append(last_buy_price)
 
-        if int(hist[2]) > 0:
-            buy(x, int(hist[2]))
-            last_buy_price = int(hist[2]) #최근 구매 가격
-            current_buy_day = int(hist[0]) #최근 거래 날짜
-
-        elif int(hist[2]) < 0:
-            making_withdraw(-int(hist[2]))
-
-        if last_buy_day+y <= current_day and last_buy_day+y <= 30:
-            making_mileage(last_buy_price)
-            last_buy_day = current_buy_day #다음 차례 넣을 수 있다.
-
+        elif int(pay_list[2]) < 0:
+            making_withdraw(-int(pay_list[2]))
+    else :
+        while len(buy_days_queue) > 0 and buy_days_queue[0] + y <= 30:
+            buy_days_queue.popleft()
+            target_price = buy_price_queue.popleft()
+            mileage += int(target_price*0.1)
 
     return [total_charge, total_withdraw, point, mileage]
 
-print(solver(10000,3,['12 12:00 23000', '16 23:00 12000']))
+print('총충전금액, 총출금금액, 남은 포인트, 남은 마일리지')
+print(solver(10000,3,['12 12:00 23000', '16 23:00 12000', '16 21:00 8000', '28 21:00 10000' ]))
 
