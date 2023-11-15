f6f1819dae502962ee9fcfebc1c2ae2a45b40684
97danielj <97danielj@gmail.com>
2023-11-15 16:29:53 +0900

    go over gcd, lcm 231115

--- ThisIsCodingTest/재귀함수/최대공약수 계산(유클리드 호제법).py
+++ ThisIsCodingTest/재귀함수/최대공약수 계산(유클리드 호제법).py
@@ -1,13 +1,16 @@
+
+def gcd(A, B):
+    while B > 0:
+        A, B = B, A % B
+    return A
+
+def lcm(A,B):
+    return A * B / gcd(A, B)
+
 numbers = list(map(int,input().split()))
 
 A = numbers[0]
 B = numbers[1]
 
-def gcd(A, B):
-    if A%B ==0:
-        return B
-    else:
-        return gcd(B, A % B)
-
-
-print(gcd(192,162))
+print(f"gcd : {gcd(A, B)}")
+print(f"lcm : {int(lcm(A, B)) }")
